325d6de58babe6cd51bcef0bc44632d1717e01cc
mouthzipper <ramos.jeromeh@gmail.com>
2015-03-18 16:11:48 +0800

    create gh-page

--- src/client/app/blocks/logger/logger.module.js
+++
@@ -1,5 +0,0 @@
-( function ( angular ) {
-    'use strict';
-
-    angular.module('blocks.logger', []);
-} ) ( angular );
--- src/client/app/detail/detail.module.js
+++
@@ -1,8 +0,0 @@
-( function () {
-	'use strict';
-
-	angular.module( 'app.detail', [
-		'app.core',
-		'app.data'
-	]);
-})();
